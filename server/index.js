1a56f6aae97d3dbb8cc620cc6557d1b1341a9405
Jake Enger <99782876+jake-enger@users.noreply.github.com>
2022-05-28 15:25:17 -0600

    hopefully better work on server, soon to be started on css and html

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const cors = require('cors')
 
 const app = express()
@@ -7,8 +6,18 @@ const app = express()
 app.use(express.json())
 
 app.use(cors())
+app.use(express.json())
 
 app.listen(4000, () => console.log('Server running on port 4000'))
 
-//app.get('api/v2/pokeon/{"rayquaza"}')
-    //god i hope i do this right
+app.get("/api/myPokemon", (req, res) => {
+    const myPokemon = [Blaziken,
+    Shedinja,
+    Kyogre,
+    Latias,
+    Metagross,
+    Rayquaza];
+res.status(200).send(myPokemon)
+});
+
+
