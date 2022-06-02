4ffe44025ad5f731f1708fcec96cfa49ac37ea46
Jake Enger <99782876+jake-enger@users.noreply.github.com>
2022-06-01 19:39:54 -0600

    fixed some syntax and layout, but getting an ERR_CONNECTION_REFUSED and not sure how to fix

--- server/index.js
+++ server/index.js
@@ -3,37 +3,36 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(express.json())
-
 app.use(cors())
 app.use(express.json())
 
-app.listen(4000, () => console.log('Server running on port 4000'))
 
 app.get("/api/myPokemon", (req, res) => {
-    const myPokemon = [Blaziken,
-    Shedinja,
-    Kyogre,
-    Latias,
-    Metagross,
-    Rayquaza];
+    const myPokemon = ['Blaziken',
+    'Shedinja',
+    'Kyogre',
+    'Latias',
+    'Metagross',
+    'Rayquaza'];
 
     let randomIndex = Math.floor(Math.random() * myPokemon.length);
     let friendlyPokemon = myPokemon[randomIndex];
 
-    res.status(200).send(myPokemon);
+    res.status(200).send(myPokemon)
 });
 
 app.get("/api/eliteFour", (req, res) => {
     const eliteFour = [
-        Swellow,
-        Shroomish,
-        Numel,
-        Marshtomp
+        'Swellow',
+        'Shroomish',
+        'Numel',
+        'Marshtomp'
     ];
 
     let randomIndex = Math.floor(Math.random() * eliteFour.length);
     let enemyPokemon = eliteFour[randomIndex];
 
-    res.status(200).send(eliteFour);
+    res.status(200).send(eliteFour)
 });
+
+app.listen(4000, () => console.log('Server running on port 4000'))
